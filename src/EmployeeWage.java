72dfc8d3e50a4f5cabb83962e8388400d6b546bf
Surya2742 <boby7724284@gmail.com>
2022-08-05 20:58:07 +0530

    added: Employee Absent or Present

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -1,7 +1,7 @@
 public class EmployeeWage {
     public static void main(String[] args) {
         int fullTimeWorking = 1;
-        double empCheck = Math.floor(Math.random() * 2 );
+        double empCheck = Math.floor(Math.random() * 2);
         if (empCheck == fullTimeWorking)
             System.out.println("Employee is Present");
         else
