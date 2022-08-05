3fefe33dfaa547107a0ef4134c06ca57391b80e1
Surya2742 <boby7724284@gmail.com>
2022-08-06 00:08:35 +0530

    refactor: Switch case statement used to refactor

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -1,21 +1,30 @@
 public class EmployeeWage {
     public static void main(String[] args) {
-        int fullTimeWorking = 1;
+        final int fullTimeWorking = 1;
         int fullTimeHour = 8;
-        int isPartTime = 2;
+        final int isPartTime = 2;
         int partTimeHour = 4;
         int wagePerHour = 20;
-        double empCheck = Math.floor(Math.random() * 3);
-        if (empCheck == fullTimeWorking) {
-            System.out.println("Employee is Present");
-            System.out.println("Working time is " + fullTimeHour + " and the salary is " + fullTimeHour * wagePerHour);
-        }
-        else if(empCheck == isPartTime) {
-            System.out.println("Employee is Working Part Time");
-            System.out.println("Working time is " + partTimeHour + " and the salary is " + partTimeHour * wagePerHour);
-        }
-        else {
-            System.out.println("Employee is Absent");
+        int empCheck = (int) Math.floor(Math.random() * 3);
+        switch (empCheck) {
+
+            case fullTimeWorking: {
+                System.out.println("Employee is Present");
+                System.out.println("Working time is " + fullTimeHour + " and the salary is " + fullTimeHour * wagePerHour);
+                break;
+            }
+
+            case isPartTime: {
+                System.out.println("Employee is Working Part Time");
+                System.out.println("Working time is " + partTimeHour + " and the salary is " + partTimeHour * wagePerHour);
+                break;
+            }
+
+            default: {
+                System.out.println("Employee is Absent");
+                break;
+            }
+
         }
     }
 }
