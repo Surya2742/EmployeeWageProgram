9ccb54ffaa6bf706ef75e5f4f6880add8f6cb97c
Surya2742 <boby7724284@gmail.com>
2022-08-11 23:04:03 +0530

    added: Class method with function parameters with multiple Companies

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -1,14 +1,24 @@
 public class EmployeeWage {
     private final int fullTimeWorking = 1;
     private final int isPartTime = 2;
-    void EmpWageCalculateMethod() {
-        int wagePerHour = 20;
-        int workingDayInMonth = 20;
-        int maxWorkingHrs = 100;
+    final String companyName;
+    final int wagePerHour;
+    final int workingDayInMonth;
+    final int maxWorkingHrs;
+    public EmployeeWage(String companyName, int wagePerHour, int workingDayInMonth, int maxWorkingHrs) {
+    this.companyName = companyName;
+    this.wagePerHour = wagePerHour;
+    this.workingDayInMonth = workingDayInMonth;
+    this.maxWorkingHrs = maxWorkingHrs;
+    }
 
+    public int EmpWageCalculateMethod() {
         int empHrs;
         int totalEmpHrs = 0;
         int totalWorkingDays = 0;
+        int fullTimeCounter = 0;
+        int partTimeCounter = 0;
+        int absentCounter = 0;
 
         while (totalEmpHrs <= maxWorkingHrs && totalWorkingDays < workingDayInMonth) {
             totalWorkingDays++;
@@ -16,26 +26,34 @@ public class EmployeeWage {
             switch (empCheck) {
                 case fullTimeWorking: {
                     empHrs = 8;
+                    fullTimeCounter++;
                     break;
                 }
                 case isPartTime: {
                     empHrs = 4;
+                    partTimeCounter++;
                     break;
                 }
                 default: {
                     empHrs = 0;
+                    absentCounter++;
                     break;
                 }
             }
             totalEmpHrs = totalEmpHrs + empHrs;
         }
-        int monthlyWage = totalEmpHrs * wagePerHour;
-        System.out.println("Total Employee Hours" + totalEmpHrs);
-        System.out.println("Total Working Days" + totalWorkingDays);
-        System.out.println("Employee Monthly Wage = " + monthlyWage);
+
+        System.out.println("Full Time Attendance : " + fullTimeCounter);
+        System.out.println("Part Time Attendance : " + partTimeCounter);
+        System.out.println("Absent count : " + partTimeCounter);
+        System.out.println("Total Employee Hours : " + totalEmpHrs);
+        System.out.println("Total Working Days : " + totalWorkingDays);
+        return totalEmpHrs * wagePerHour;
     }
     public static void main(String[] args) {
-        EmployeeWage em = new EmployeeWage();
-        em.EmpWageCalculateMethod();
+        EmployeeWage google = new EmployeeWage("Alphabet Inc. ", 100, 20, 100);
+        System.out.println("Total Employee Wage for " + google.companyName + " is : $" + google.EmpWageCalculateMethod() + "\n\n");
+        EmployeeWage saiSystem = new EmployeeWage("SaiSystem Technology pvt. ltd.", 80, 24, 120);
+        System.out.println("Total Employee Wage for " + saiSystem.companyName + " is : $" + saiSystem.EmpWageCalculateMethod());
     }
 }
